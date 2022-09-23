bc2ad06e4151e5629db7a8de9f2ffbab2b7c209b
Yoshiya Hinosawa <stibium121@gmail.com>
2022-09-23 18:12:11 +0900

    update

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 This is a example script of server-side rendering unocss in Deno.
 
 See [ssr.ts](./ssr.ts) for details.
+
+Or try the command:
+```
+deno run https://raw.githubusercontent.com/kt3k/unocss-deno-ssr-example/main/ssr.ts
+```
--- ssr.ts
+++ ssr.ts
@@ -1,13 +1,20 @@
 import { createGenerator } from "https://esm.sh/@unocss/core@0.45.22";
 import presetUno from "https://esm.sh/@unocss/preset-uno@0.45.22";
+import { green } from "https://deno.land/std@0.157.0/fmt/colors.ts";
 
 // Creates Uno generator
 const uno = createGenerator({ presets: [presetUno()] });
 
-// Generates css based on input (here the only input is "ml-1")
-var { css } = await uno.generate("ml-1", { preflights: false });
-console.log(css);
+async function generate(input: string) {
+  // Generates css based on the input
+  const { css } = await uno.generate(input, { preflights: false });
+  console.log();
+  console.log(green("Input:"));
+  console.log(input);
+  console.log(green("Output:"));
+  console.log(css);
+}
 
-// The input is "ml-1 ml-2 ml-3"
-var { css } = await uno.generate("ml-1 ml-2 ml-3", { preflights: false });
-console.log(css);
+await generate("ml-1");
+await generate("ml-1 ml-2 ml-3");
+await generate("border border-t border-gray-400");
