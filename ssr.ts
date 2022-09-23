b48824ba2e80906d7dc27b1f9afe4072a0c5f972
Yoshiya Hinosawa <stibium121@gmail.com>
2022-09-23 18:06:38 +0900

    update

--- ssr.ts
+++ ssr.ts
@@ -1,6 +1,13 @@
 import { createGenerator } from "https://esm.sh/@unocss/core@0.45.22";
 import presetUno from "https://esm.sh/@unocss/preset-uno@0.45.22";
+
+// Creates Uno generator
 const uno = createGenerator({ presets: [presetUno()] });
-const res = await uno.generate(["ml-1"], { preflights: false });
-const { css } = res;
+
+// Generates css based on input (here the only input is "ml-1")
+var { css } = await uno.generate("ml-1", { preflights: false });
+console.log(css);
+
+// The input is "ml-1 ml-2 ml-3"
+var { css } = await uno.generate("ml-1 ml-2 ml-3", { preflights: false });
 console.log(css);
